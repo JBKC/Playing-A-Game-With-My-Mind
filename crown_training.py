f9f6543e9706cb8f43bef62f599ea101db93d8d9
James Bannon <121674801+JBKC@users.noreply.github.com>
2024-07-04 02:32:00 +0100

    edited training file

--- crown_training.py
+++ crown_training.py
@@ -1,3 +1,41 @@
 '''
 File to map EEG kinetic thought training to button outputs
 '''
+
+from neurosity import NeurositySDK
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+def initialise():
+    '''
+    Initialise Crown for receiving data
+    :return: neurosity: instance of NeurositySDK
+    '''
+
+    try:
+        # get environment variables
+
+        # initialise NeurositySDK
+        neurosity = NeurositySDK({
+            "device_id": os.getenv("DEVICE_ID"),
+            "timesync": True
+
+        })
+
+        # login to Neurosity account
+        neurosity.login({
+            "email": os.getenv("EMAIL"),
+            "password": os.getenv("PASSWORD")
+        })
+
+        print("Successfully logged in")
+        return neurosity
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+initialise()
