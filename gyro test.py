9a277a65b5a2dd7f7c37666399b79c6f1c0a1bef
James Bannon <121674801+JBKC@users.noreply.github.com>
2024-08-02 00:58:29 -0400

    2D functional gyro

--- gyro test.py
+++ gyro test.py
@@ -2,63 +2,44 @@
 Translate gyro inputs into pygame onscreen input
 '''
 
-from neurosity import NeurositySDK
 import asyncio
-from dotenv import load_dotenv
-import numpy as np
-import os
-import crown_realtime_processing
-import joblib
 import collections
 import pygame
 import serial
-import tracemalloc
-import time
-import cProfile
-import pstats
-
-import io
-
 
 ################################
-
-async def put_buffer(buffer, data):
-    await buffer.put(data)
 
 # coroutine 1: get gyro data (producer)
 async def gyro_stream(buffer, ser):
 
-    loop = asyncio.get_running_loop()
-
     try:
         while True:
             data = ser.readline().decode('utf-8').strip().split(',')
-            data = data[2]
-            asyncio.run_coroutine_threadsafe(put_buffer(buffer, data), loop)
+            buffer.append((float(data[0]),float(data[1]))) # save (x,y) gyro data
 
             # yield control back to event loop after callback has been executed
             await asyncio.sleep(0)
 
     # stop stream when program exits
     finally:
-        await buffer.put(None)          # signal end of stream
+        buffer.append(None)          # signal end of stream
 
 # coroutine 2: display orientation data (consumer)
+
 async def pygame_display(buffer):
     '''
     :param: buffer: of incoming gyro data
     '''
-
 
     pygame.init()
     width, height = 600, 600
     screen = pygame.display.set_mode((width, height))
 
     dot_radius = 10
-    dot_color = (255, 0, 0)  # Red color
+    dot_color = (255, 0, 0)  # red
     BLACK = (0, 0, 0)
 
-    data = None
+    clock = pygame.time.Clock()
     running = True
 
     while running:
@@ -66,26 +47,25 @@ async def pygame_display(buffer):
             if event.type == pygame.QUIT:
                 running = False
 
-        # check for updates in the queue
-        if not buffer.empty():
-            data = await buffer.get()
-
         screen.fill(BLACK)
 
-        if data is not None:
+        if buffer:
+            data = buffer[-1]  # Get the most recent data
+            buffer.clear()  # Clear the buffer
+            print(data)
 
-            x_distance = screen.get_width() / 2 + (30 * float(data))
-            print(x_distance)
+            if data is not None:
+                # convert gyro data to onscreen image
+                x_dist = int(width / 2 + (30 * data[0]))
+                y_dist = int(height / 2 + (30 * data[-1]))
+                pygame.draw.circle(screen, dot_color, (x_dist, y_dist), dot_radius)
 
-            # Draw the dot
-            pygame.draw.circle(screen, dot_color, (x_distance, screen.get_height() / 2), dot_radius)
-
-            # Update the display
-            pygame.display.flip()
-
-        await asyncio.sleep(0)  # yield control to the event loop
+        pygame.display.flip()
+        clock.tick(5)  # fps limit
+        await asyncio.sleep(0)
 
     pygame.quit()
+
 
 
 async def main():
@@ -93,7 +73,7 @@ async def main():
     arduino_port = '/dev/tty.usbmodem14201'
     baud_rate = 115200
 
-    buffer = asyncio.Queue()      # create buffer
+    buffer = collections.deque(maxlen=1)
 
     # Open the serial port
     ser = serial.Serial(arduino_port, baud_rate)
