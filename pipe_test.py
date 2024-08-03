20f17598b616a122e7ff74e3d96d97e2d0425fdc
James Bannon <121674801+JBKC@users.noreply.github.com>
2024-08-03 01:32:05 -0400

    full control of mario kart steering

--- pipe_test.py
+++ pipe_test.py
@@ -1,12 +1,14 @@
 '''
-Send realtime gyro inputs to pipeline to be used by gaming emulator
+Send realtime accelerometer inputs to pipeline to be used by gaming emulator
+Designed for Mario Kart Wii on Dolphin Emulator
+To be eventually replaced by EEG inputs
 '''
 
-import os
-import time
 import serial
-import fcntl
 
+# // {PRESS, RELEASE} {A, B, X, Y, Z, START, L, R, D_UP, D_DOWN, D_LEFT, D_RIGHT}
+# // SET {L, R} [0, 1]
+# // SET {MAIN, C} [0, 1] [0, 1]
 
 fifo_path = "/Users/jamborghini/Library/Application Support/Dolphin/Pipes/fifo_pipe"
 arduino_port = '/dev/tty.usbmodem14201'
@@ -16,28 +18,60 @@ baud_rate = 115200
 ser = serial.Serial(arduino_port, baud_rate)
 
 try:
-    with open(fifo_path, 'w') as fifo:
+    with (open(fifo_path, 'w') as fifo):
         while True:
             data = ser.readline().decode('utf-8').strip().split(',')
 
-            # initialize command
-            command = ''
+            # initialize commands
+            commands = ''
             print(data)
 
-
-            # get individual gyro data
+            # get individual accelerometer data
             x_data = float(data[0])
             y_data = float(data[1])
-            z_data = float(data[2])
+            # z_data = float(data[2])
 
+            # if y_data < -5:
+            #     commands = {
+            #         'PRESS A'}
+            # if y_data > 5:
+            #     commands = {
+            #         'PRESS B'}
 
-            # if z_data > 0:
-            #     command = 'PRESS A'
-            #
-            # if command:
-            #     fifo.write(command + '\n')
-            #     fifo.flush()
-            #     print("COMMAND SENT")
+            # menu commands
+            if x_data > 5:
+                commands = {
+                    'PRESS D_DOWN',
+                    'SET MAIN 1 0.5',
+                }
+            if x_data < -5:
+                commands = {
+                    'PRESS D_UP',
+                    'SET MAIN 0 0.5',
+                }
+
+            # steering control
+            for x in range()
+
+            if -2 < x_data < 2:
+                commands = {
+                    'RELEASE D_DOWN',
+                    'RELEASE D_UP',
+                    'SET MAIN 0.5 0.5',
+                }
+
+            # if -2 < y_data < 2:
+            #     commands = {
+            #         'RELEASE A',
+            #         'RELEASE B',
+            #     }
+
+            if commands:
+                for command in commands:
+                    fifo.write('PRESS A' + '\n')
+                    fifo.write(command + '\n')
+                    fifo.flush()
+                    print(f'{command} SENT')
 
 
 finally:
@@ -46,6 +80,6 @@ finally:
 
 
 '''
-- calibrate gyro by holding steady for 5 seconds in "resting" state
-- movements to the emulator are a function of how much the gyro has moved from the calibrated state
+- calibrate accelerometer by holding steady for 5 seconds in "resting" state
+- movements to the emulator are a function of how much the accelerometer has moved from the calibrated state
 '''
