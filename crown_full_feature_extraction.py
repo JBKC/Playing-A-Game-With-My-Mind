3619ac8a43980051866cc13fd7e32c989d4d92b4
James Bannon <121674801+JBKC@users.noreply.github.com>
2024-08-15 05:56:05 +0100

    adjusting filter banks

--- crown_full_feature_extraction.py
+++ crown_full_feature_extraction.py
@@ -14,13 +14,18 @@ def bpass_filter(data, lowcut, highcut, fs, order=5):
     high = highcut / nyquist
     b, a = butter(order, [low, high], btype='band')
 
-    return filtfilt(b, a, data, axis=2)
+    return filtfilt(b, a, data)
 
-
+# def notch_filter(data, )
 
 def normalise(signal):
     # Z-score normalisation
     return (signal - np.mean(signal)) / np.std(signal)
+
+def notch_filter(data, fs, f0, quality_factor):
+    b, a = scipy.signal.iirnotch(f0, quality_factor, fs)
+
+    return filtfilt(b, a, data)
 
 
 def compute_psd(tensor):
@@ -33,13 +38,8 @@ def plot_frequency_domain(freqs, P1):
 
     channel_names = ['CP3', 'C3', 'F5', 'PO3', 'PO4', 'F6', 'C4', 'CP4']
 
-    # # Create subplots
-    # fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 5))
-
     # plot frequency domain
     plt.plot(freqs, P1, color='black', linewidth=1)
-    # plt.set_ylim(0, 1)
-    # plt.set_xlim(0, 30)
 
     plt.tight_layout()
     plt.show()
@@ -52,7 +52,19 @@ def main(X1, X2):
     print(f'Class 1 trials: {X1.shape[0]}')
     print(f'Class 2 trials: {X2.shape[0]}')
 
-    X1 = X1[0][0]
+    X1 = X1[0][1]
+
+    plt.plot(X1)
+    plt.show()
+
+    # apply notch filter for power noise
+    fs = 256
+    f0 = 50                         # frequency to be removed
+    quality_factor = 30
+
+    X1 = notch_filter(X1, fs, f0, quality_factor)
+
+    # X1 = bpass_filter(X1, 10, 30, fs=256, order=5)
 
     # break out frequency components
     freqs_raw, P1 = compute_psd(X1)
