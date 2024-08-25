d8598a7da1b4e24635ec6648fee2d05fef997279
JBKC <jmbannon97@gmail.com>
2024-08-25 19:14:02 +0100

    adding phase lag index

--- coherence_analysis.py
+++ coherence_analysis.py
@@ -11,37 +11,53 @@ def phase_lag_index(tensor):
     Calculates the PLI over trials
     '''
 
+    def compute_pli(signal1, signal2):
+
+        # calculate instantaneous phase angle using HT
+        analytic1 = scipy.signal.hilbert(signal1)
+        analytic2 = scipy.signal.hilbert(signal2)
+
+        ip1 = np.angle(analytic1)
+        ip2 = np.angle(analytic2)
+
+        # phase angle difference
+        diffs = ip1-ip2
+        pli = np.abs(np.mean(np.sign(np.imag(np.exp(1j * diffs)))))
+
+        return pli
+
     # PLIs for each channel combination
-    plis = np.zeros((tensor.shape[1], tensor.shape[1]))
+    pli_matrix = np.zeros((tensor.shape[1], tensor.shape[1]))
 
-    # array of phase angle differences for each trial
-    trial_diffs = np.zeros((tensor.shape[0], tensor.shape[-1]))
+    # iterate over all possible combinations of channels
+    for ch1 in range(tensor.shape[1]):
+        for ch2 in range(tensor.shape[1]):
 
-    for channel in range(tensor.shape[1]):
-        for trial in range(tensor.shape[0]):
+            # array of plis for each trial - shape (no.trials)
+            trial_plis = np.zeros((tensor.shape[0]))
 
-            # take examples from different electrodes:
-            signal1 = tensor[trial, channel, :]
-            signal2 = tensor[trial, channel+1, :]
+            for trial in range(tensor.shape[0]):
 
-            # calculate instantaneous phase angle using HT
-            analytic1 = scipy.signal.hilbert(signal1)
-            analytic2 = scipy.signal.hilbert(signal2)
+                signal1 = tensor[trial, ch1, :]
+                signal2 = tensor[trial, ch2, :]
 
-            ip1 = np.angle(analytic1)
-            ip2 = np.angle(analytic2)
+                # calculate pli for a single trial for given channel combination
+                pli = compute_pli(signal1, signal2)
 
-            # record running differences
-            trial_diffs[trial, :] = abs(ip1-ip2)
+                trial_plis[trial] = pli
 
-    # average over trials
-    av_diffs = np.mean(trial_diffs, axis=0)
-    print(av_diffs)
+            # average PLI over trials for given channel combination
+            av_pli = np.mean(trial_plis)
+            # append to PLI matrix
+            pli_matrix[ch1,ch2] = av_pli
 
-    pli = np.abs(np.mean(np.sign(np.imag(np.exp(1j * av_diffs)))))
-    print(pli)
+    print(pli_matrix)
+    print(pli_matrix.shape)
 
-
+    # visualise PLI between channels
+    plt.imshow(pli_matrix, cmap='viridis', interpolation='nearest')
+    plt.colorbar()  # Add a color bar to indicate the intensity scale
+    plt.show()
 
 def main(dict, band):
     '''
